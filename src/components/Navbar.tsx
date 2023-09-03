eb7daf6841a97bbd3c8835127adeb3b3997735ad
Harshit Jain <harshitjain5june@gmail.com>
2023-09-03 13:05:06 +0530

    login & signup btn modified on navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -17,39 +17,44 @@ function Navbar() {
     history('/');
   }
 
-  const handleShowLogin = () =>{
+  const handleShowLogin = () => {
     setShowLogin(true);
   }
 
-  const handleShowSignup = () =>{
+  const handleShowSignup = () => {
     setShowSignUp(true);
   }
 
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container-fluid">
-        <Link to={'/'} className="navbar-brand fs-3 fst-italic" >GoFood</Link>
+        <Link to={'/'} className="navbar-brand fs-3 fst-italic">GoFood</Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <Link to={'/'} className="nav-link active" aria-current="page" >Home</Link>
+              <Link to={'/'} className="nav-link active fs-6 fw-bold" aria-current="page">Home</Link>
             </li>
-            <li className="nav-item">
-              <Link to={'/login'} className="nav-link" onClick={() => {setShowLogin(true)}}>Login</Link>
-              {showLogin && <Login onOpenSignUp={handleShowSignup} onClose={handleCloseLogin} />}
-            </li>
-            <li className="nav-item">
-              <Link to={'/signup'} className="nav-link" onClick={() => {setShowSignUp(true)}}>SignUp</Link>
-              {showSignUp && <SignUp onOpenLogin={handleShowLogin}  onClose={handleCloseSignUp} />}
-            </li>
-          </ul>
 
+            {localStorage.getItem("authToken") ? <li className="nav-item">
+              <Link to={'/'} className="nav-link active fs-6 fw-bold" aria-current="page">My Orders</Link>
+            </li> : ""}
+          </ul>
+          <div className="d-flex align-items-center">
+            {!localStorage.getItem("authToken") ? <><Link to={'/login'} className="btn bg-white fw-bold text-success mx-1" onClick={() => { setShowLogin(true) }}>Login</Link>
+              <Link to={'/signup'} className="btn bg-white fw-bold text-success mx-1" onClick={() => { setShowSignUp(true) }}>SignUp</Link>
+            </> : <Link to={'/'} className="btn bg-white fw-bold text-success mx-1">Logout</Link>
+            }
+            {showSignUp && <SignUp onOpenLogin={handleShowLogin} onClose={handleCloseSignUp} />}
+            {showLogin && <Login onOpenSignUp={handleShowSignup} onClose={handleCloseLogin} />}
+          </div>
         </div>
       </div>
     </nav>
+
+
   )
 }
 
